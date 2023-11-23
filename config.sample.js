c0ea40e2619b0ff319744ccbdcb4fdd6c1d840e9
Jim Chen <wfrtmouse@gmail.com>
2023-11-23 09:10:57 +0800

    change config sample

--- config.sample.js
+++ config.sample.js
@@ -1,20 +1,20 @@
 module.exports = {
   sites: [
     {
-      name: "holmes server",
-      url: "http://127.0.0.1:3040/api",
+      name: "test server",
+      url: "http://127.0.0.1:3040",
       interval: 60000,
       run: {
-        base_dir: "D:\\server\\holmes\\server",
-        cmd: "run.bat",
+        base_dir: "/user/home/test",
+        cmd: "npm start",
       },
     },
     {
-      name: "oneapi server",
-      url: "http://127.0.0.1:3000/api",
+      name: "api server",
+      url: "http://127.0.0.1:8000/api",
       interval: 60000,
       run: {
-        base_dir: "D:\\Server\\OneAPI2",
+        base_dir: "D:\\Server\\API",
         cmd: "run.bat",
       },
     },
